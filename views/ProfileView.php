152f612bf1769a957b8076bfd67ffb75e86e0e61
Petr Illík <ExitakCZ@seznam.cz>
2017-06-10 16:38:41 +0200

    created profile pages

--- views/ProfileView.php
+++ views/ProfileView.php
@@ -7,6 +7,23 @@
       header('Location: landing-page');
       die();
   }
+  	$userName = self::query("SELECT username FROM users WHERE ID=:ID", array(':ID'=>self::isLoggedIn()))[0]['username'];
+    if(!isset($_GET['user']))
+    {
+		header('Location: profile?user='.$userName);
+		die();
+	}
+	if(!self::query("SELECT * FROM users WHERE username=:un", array(':un'=>$_GET['user'])))
+	{
+	   header('Location: home');
+       die();
+	}
+	$playerId = self::query("SELECT ID FROM users WHERE username=:un", array(':un'=>$_GET['user']))[0]['ID'];
+	if(!self::query("SELECT * FROM students WHERE user_id=:ui", array(':ui'=>$playerId)))
+	{
+	   header('Location: home');
+       die();
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -62,15 +79,85 @@
             </header>
             <div class="mdl-layout__drawer">
                 <span class="mdl-layout-title">Menu</span>
-                <button class="mdl-button mdl-js-button " onclick="goTo('home');">
+                <button class="mdl-button mdl-js-button " onClick="goTo('home');">
                     Domů
                 </button>
-                <button class="mdl-button mdl-js-button " onclick="goTo('logout');">
+                <button class="mdl-button mdl-js-button " onClick="goTo('logout');">
                     Odhlásit se
                 </button>
             </div>
             <main class="mdl-layout__content">
-                <div id="printContent">
+                <!-- Wide card with share menu button -->
+<div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--12-col">
+  <style>
+.demo-card-wide.mdl-card {
+  width: 100%;
+}
+.demo-card-wide > .mdl-card__title {
+  color: #fff;
+  text-shadow: 1px 1px black;
+  height: 176px;
+  background: url('http://pre05.deviantart.net/690e/th/pre/f/2015/018/5/4/overwatch___mercy_2_by_sonellion-d8eczkm.jpg') center / cover;
+}
+.demo-card-wide > .mdl-card__menu {
+  color: #fff;
+}
+</style>
+
+<div class="demo-card-wide mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title" id="backgroundTag">
+    <h2 class="mdl-card__title-text" id="nameTag">Welcome</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+<!-- Two Line List with secondary info and action -->
+<style>
+.demo-list-two {
+  width: 300px;
+}
+</style>
+
+<ul class="demo-list-two mdl-list">
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">attach_money</i>
+      <span>Peníze</span>
+      <span class="mdl-list__item-sub-title" id="moneyTag">0</span>
+    </span>
+    </li>
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">school</i>
+      <span>Kredity</span>
+      <span class="mdl-list__item-sub-title" id="creditsTag">0</span>
+    </span>
+    </li>
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">loyalty</i>
+      <span>Zdraví</span>
+      <span class="mdl-list__item-sub-title" id="healthTag">0</span>
+    </span>
+     </li>
+	 <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">lightbulb_outline</i>
+      <span>Zdravý rozum</span>
+      <span class="mdl-list__item-sub-title" id="sanityTag">0</span>
+    </span>
+    </li>
+</ul>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+  </div>
+  <div class="mdl-card__menu">
+  </div>
+</div>
+  </div>
+</div>
+
+
+				<div id="printContent">
                 </div>
             </main>
         </div>
@@ -88,14 +175,13 @@
             $("#usernameTabTitle").html(username);
           }
 
-          function LoadGameValues(c,m,h,s,a,sc,hc){
-            $('#profileIMG').attr('src', a);
-            $('#cval').html(c);
-            $('#mval').html(m + ' CZK');
-            $('#hval').html(h + '%');
-            $('#sval').html(s + '%');
-            $('#hcval').html(hc + ' CZK / 100%');
-            $('#scval').html(sc + ' CZK / 100%');
+          function LoadGameValues(c,m,h,s,a,u){
+            $('#backgroundTag').css('background-image', 'url(' + a + ')');
+            $('#creditsTag').html(c);
+            $('#moneyTag').html(m + ' CZK');
+            $('#healthTag').html(h + '%');
+            $('#sanityTag').html(s + '%');
+			 $("#nameTag").html(u);
           }
 
           function AddToPage(html){
